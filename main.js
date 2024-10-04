972053570486833c55c32a3c44b8e4f1303f1c49
valikrozenkov <rozenkov.valik@gmail.com>
2024-10-04 13:27:09 +0300

    feat(/develop-sections): finished landing page

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll('.basic-card');
     
-    // Задать классы по умолчанию для второй карточки
-    const defaultCard = cards[1]; // Вторая карточка
+    const defaultCard = cards[1];
     defaultCard.classList.add('choosen-card');
     defaultCard.querySelector('.card-header').classList.add('header-choosen-card');
     
-    // Добавить события для всех карточек
     cards.forEach(card => {
       card.addEventListener('mouseenter', () => {
-        // Убираем классы со всех карточек перед добавлением
         removeClasses();
         card.classList.add('choosen-card');
         card.querySelector('.card-header').classList.add('header-choosen-card');
       });
   
       card.addEventListener('mouseleave', () => {
-        // Можно вернуть классы на карточку по умолчанию, если нужно
         removeClasses();
         defaultCard.classList.add('choosen-card');
         defaultCard.querySelector('.card-header').classList.add('header-choosen-card');
